dfc080e0f7f4e3c433ce2b98942e42c77c5269ab
chengye233 <lijiacheng.ye@gmail.com>
2018-11-25 15:20:03 +0800

    添加mMediaPlayer.setOnCompletionListener释放资源。

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,6 +17,16 @@ public class FamilyActivity extends AppCompatActivity {
      */
     private MediaPlayer mMediaPlayer;
 
+    /**
+     * 异步回调 释放MediaPlayer资源
+     */
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,10 +59,38 @@ public class FamilyActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word currentWord = words.get(position);
                 if (currentWord.hasSong()) {
+                    // 播放前也释放资源
+                    releaseMediaPlayer();
+
                     mMediaPlayer = MediaPlayer.create(FamilyActivity.this, currentWord.getSongResourceId());
                     mMediaPlayer.start();
+
+                    // 播放完成后异步回调 释放MediaPlayer资源
+                    mMediaPlayer.setOnCompletionListener(onCompletionListener);
+
                 }
             }
         });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+//            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,16 @@ public class NumbersActivity extends AppCompatActivity {
      */
     private MediaPlayer mMediaPlayer;
 
+    /**
+     * 异步回调 释放MediaPlayer资源
+     */
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
 
     /**
      * 构造器 MainActivity点击时构造
@@ -55,11 +65,39 @@ public class NumbersActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word currentWord = words.get(position);
                 if (currentWord.hasSong()) {
+                    // 播放前也释放资源
+                    releaseMediaPlayer();
+
                     mMediaPlayer = MediaPlayer.create(NumbersActivity.this, currentWord.getSongResourceId());
                     mMediaPlayer.start();
+
+                    // 播放完成后异步回调 释放MediaPlayer资源
+                    mMediaPlayer.setOnCompletionListener(onCompletionListener);
+
                 }
             }
         });
 
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+//            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
     }
 }
