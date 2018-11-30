23f30d5e7ba755c67a3a5f38b5a0fa4bf25645b8
chengye233 <lijiacheng.ye@gmail.com>
2018-11-30 21:04:13 +0800

    添加FragmentAdapter。

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /**
+     *  为了调用 getString()方法
+     */
+    private Context mContext;
+
+    public CategoryAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            case 3:
+                return mContext.getString(R.string.category_phrases);
+            default:
+                return null;
+        }
+    }
+}
